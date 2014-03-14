f94d285c0d1bbe2b8f379a4946d884832eb485c1
anand <flt.anandkumar@gmail.com>
2014-03-14 14:32:05 +0530

    web-kit design and improved code

--- partner_extended/report/account_invoice_extend_webkit.py
+++ partner_extended/report/account_invoice_extend_webkit.py
@@ -18,8 +18,7 @@ class account_webkit(report_sxw.rml_parse):
             'get_page_header' : self.get_page_header,
             'get_partner_name': self.get_partner_name,
             'get_vat': self.get_vat,
-            'fill_star': self.fill_star,
-            'fill_stars': self.fill_stars,
+            'amount2words': self.amount_to_words,
         })
         self.context = context.copy()       
     def get_page_header(self):
@@ -54,25 +53,13 @@ class account_webkit(report_sxw.rml_parse):
             vat = vat[2:length]
         return vat
 
-    def fill_stars(self, amount):
-        if self.context['lang'] != "es_GT":
-            amount_word = amount_to_text(amount)
-            amount_word = amount_word.replace('Dollars','')
-            word = amount_word.upper()
-            translation = Translator(to_lang="tr").translate(word)
-            translation = translation.lower().replace('euro','TL').replace('cents','KR').replace(' ','')
-            return translation
-        else:
-            return ''
+    def amount_to_words(self, amount):
+        amount_word = amount_to_text(amount)
+        word = amount_word.upper()
+        translation = Translator(to_lang="tr").translate(word)
+        translation = translation.lower().replace('euro','TL').replace('cents','KR').replace('cent','KR').replace(' ','')
+        return translation
 
-    def fill_star(self, amount):
-        if self.context['lang'] == "es_GT":
-            word = util.num_a_letras(amount).upper()
-            translation = Translator(to_lang="tr").translate(word)
-            translation = translation.lower().replace('euro','TL').replace('cents','KR').replace(' ','')
-            return translation
-        else:
-            return ''
 report_sxw.report_sxw('report.account.invoice.webkit', 'account.invoice', '7.0-roach/partner_extended/report/account_invoice_extend_webkit_tmpl.mako', parser=account_webkit, header=False)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
